28358870cb2ec835e3c54401e8e366f9fae75be1
Sławomir Paszko <folkus@gmail.com>
2020-11-19 11:17:50 +0100

    Added metrics.

---
+++ helpers/metrics.py
@@ -0,0 +1,18 @@
+'''
+Created on 15 wrz 2020
+
+@author: spasz
+'''
+
+import math
+from . import boxes
+
+
+def MetricIOU(box1, box2):
+    ''' Calculates metric.'''
+    area1 = boxes.GetArea(box1)
+    area2 = boxes.GetArea(box2)
+    intersection = boxes.GetIntersectionArea(box1, box2)
+    if (intersection != 0):
+        return intersection / (area1+area2-intersection)
+    return 0
