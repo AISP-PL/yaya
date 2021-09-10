04f9def31f0798c62248b5994bdc72fa0020090b
Sławomir Paszko <folkus@gmail.com>
2021-09-10 14:33:02 +0200

    Removed not needed files.

--- ObjectDetectors/__init__.py
+++ ObjectDetectors/__init__.py
@@ -1,8 +1,13 @@
 import os
 
+
 def IsCuda():
     ''' Checks if GPU cuda is installed and working.'''
     if (os.system('nvcc --version') == 0):
         return True
-    
+
     return False
+
+
+def CreateDetector(detectorID=0, gpuID=0):
+    ''' Creates detector.'''
