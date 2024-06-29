2d86689a6802bdae26e9b22060ad31193d2ef6b2
Sławomir Paszko <s.paszko@aisp.pl>
2024-06-29 15:58:48 +0200

    Fixes.

--- engine/annotators/annotator_confidence_heat.py
+++ engine/annotators/annotator_confidence_heat.py
@@ -2,9 +2,11 @@
  Default annotator class.
 """
 
-from PyQt5.QtCore import QPoint
 from PyQt5.QtGui import QColor
 
+from engine.annote import Annote
+from PyQt5.QtCore import QPoint, Qt
+from engine.annote_enums import AnnoteAuthorType
 import helpers.boxes as boxes
 from helpers.QtDrawing import QDrawRectangle, QDrawText, TextAlignment
 
@@ -35,29 +37,32 @@ class AnnotatorConfidenceHeat:
     """Default annotator class."""
 
     @staticmethod
-    def Draw(self, painter, highlight=False, isConfidence=True, isLabel=True):
+    def Draw(annote: Annote, painter, highlight=False, isConfidence=True, isLabel=True):
         """Draw self."""
         width, height = painter.window().getRect()[2:]
-        x1, y1, x2, y2 = boxes.ToAbsolute(self.box, width, height)
+        x1, y1, x2, y2 = boxes.ToAbsolute(annote.box, width, height)
 
-        # Thicknes of border
-        thickness = 1
-        if highlight is True:
-            thickness = 2
+        # Pen properties  : Depends on author type
+        if annote.authorType in {AnnoteAuthorType.byHuman, AnnoteAuthorType.byHand}:
+            pen_color = Qt.black
+            pen_thickness = 3
+        elif annote.authorType == AnnoteAuthorType.byDetector:
+            pen_color = Qt.white
+            pen_thickness = 1
 
         # Brush opacity : Default 0.25
         brush_opacity: float = 0.25
 
         # Brush color
-        r, g, b = RYG_color_as_rgb(self.confidence)
+        r, g, b = RYG_color_as_rgb(annote.confidence)
         brushColor = QColor(r, g, b)
 
         # Draw rectangle box
         QDrawRectangle(
             painter,
             [QPoint(x1, y1), QPoint(x2, y2)],
-            pen=brushColor,
-            penThickness=thickness,
+            pen=pen_color,
+            penThickness=pen_thickness,
             brushColor=brushColor,
             brushOpacity=brush_opacity,
         )
@@ -65,7 +70,7 @@ class AnnotatorConfidenceHeat:
         # Text
         if isLabel or isConfidence:
             # Label text
-            label = f"{self.className}\n{self.confidence:2.0f}%"
+            label = f"{annote.className}\n{annote.confidence:2.0f}%"
 
             # Position : Center of the box
             xc = (x1 + x2) // 2
--- helpers/prefilters.py
+++ helpers/prefilters.py
@@ -4,30 +4,34 @@ Created on 18 lis 2020
 @author: spasz
 """
 
-import logging
-
 import numpy as np
 
-from . import boxes, metrics
+from engine.annote import Annote
+
+from boxes import iou
 
 
-def FilterIOUbyConfidence(annotations1, annotations2, maxIOU=0.70):
+def FilterIOUbyConfidence(
+    annotations1: list[Annote], annotations2: list[Annote], maxIOU: float = 0.70
+) -> list[Annote]:
     """
     Filter annotation1 with annotations2
     if has bigger IOU > maxIOU.
     Annotation with bigger confidence stays!
     """
+
     # Create IOU results matrix
     results = np.zeros([len(annotations1), len(annotations2)], dtype=np.float32)
     for i, annote1 in enumerate(annotations1):
         for j, annote2 in enumerate(annotations2):
             if annote1 != annote2:
-                results[i, j] = boxes.iou(annote1.GetBox(), annote2.GetBox())
+                results[i, j] = iou(annote1.GetBox(), annote2.GetBox())
 
     # Parse each row of IOU matrix
-    passed = []
+    passed: list[Annote] = []
     for i, annote in enumerate(annotations1):
         isFiltered = False
+
         for j in range(len(annotations2)):
             # If IOU >= maxIOU
             if results[i, j] >= maxIOU:
@@ -36,7 +40,7 @@ def FilterIOUbyConfidence(annotations1, annotations2, maxIOU=0.70):
                     isFiltered = True
                     break
 
-        if isFiltered == True:
+        if isFiltered is True:
             """Do nothing"""
         else:
             passed.append(annote)
